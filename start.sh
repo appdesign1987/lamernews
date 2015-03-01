fca05f668667678084c1b8a5ef67d90143fff3ae
appdesign1987 <jeroen@jeroenvd.nl>
2015-03-01 14:03:23 +0100

    updates start script so that it installs the gems

--- start.sh
+++ start.sh
@@ -1,2 +1,11 @@
 #!/usr/bin/env bash
+
+#installing gems
+gem install sinatra -v 1.4.2
+gem install redis -v 3.0.4
+gem install hiredis -v 0.4.5
+gem install json -v 1.7.7
+gem install ruby-hmac -v 0.4.4
+
+#starting application
 cd /app/lamernews && bundle exec rackup -p4567 --host 0.0.0.0
